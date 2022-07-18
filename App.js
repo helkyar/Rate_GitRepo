657e62e1634b3631313b1dc8dbc64d1551a6b242
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-07-18 14:42:00 +0200

    Add main component

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Alert,
+} from "react-native";
+import { Main } from "./components/Main";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableWithoutFeedback onPress={() => Alert.alert("Text pressed")}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </TouchableWithoutFeedback>
+      <Main />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
